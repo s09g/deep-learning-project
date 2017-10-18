8b3982c2a67f997798334cce134857115e9fc592
zhangshiqiu <zhangshiqiu1993@126.com>
2017-10-17 21:45:57 -0700

    random search

--- learn.py
+++ learn.py
@@ -28,5 +28,28 @@ def play_multiple_episodes(env, T, params):
     print("avg length: ", avg_length)
     return avg_length
 
-# def random_search(env):
-    # episode_lengths
+def random_search(env):
+    episode_lengths = []
+    best = 0
+    params = None
+    for t in range(100):
+        new_params = np.random.random(4) * 2 - 1
+        avg_length = play_multiple_episodes(env, 100, new_params)
+        episode_lengths.append(avg_length)
+
+        if avg_length > best:
+            params = new_params
+            best = avg_length
+    return episode_lengths, params
+
+def main():
+    env = gym.make('CartPole-v0')
+    episode_lengths, params = random_search(env)
+    plt.plot(episode_lengths)
+    plt.show()
+
+    print("***Final run with final weights**")
+    play_multiple_episodes(env, 100, params)
+
+if __name__ == '__main__':
+    main()
