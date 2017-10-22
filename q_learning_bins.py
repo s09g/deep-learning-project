1b5396b6a67ffc08e4b1df96d6464381c2b34f3c
zhangshiqiu <zhangshiqiu1993@126.com>
2017-10-21 21:43:04 -0700

    functions

--- q_learning_bins.py
+++ q_learning_bins.py
@@ -6,5 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as import pd
 from datetime import datetime
 
-# def build_state(features):
-    
+def build_state(features):
+    return int("".join(map(lambda feature : str(int(feature)), features)))
+
+def to_bin(value, bins):
+    return np.digitize(x=[valie], bins=bins)[0]
