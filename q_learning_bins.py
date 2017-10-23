b85aa8601160fa414e69f3cbe24007df7e12e46b
zhangshiqiu <zhangshiqiu1993@126.com>
2017-10-23 10:14:41 -0700

    FeatureTransformer

--- q_learning_bins.py
+++ q_learning_bins.py
@@ -14,10 +14,19 @@ def to_bin(value, bins):
 
 class FeatureTransformer:
     def __init__(self):
-        pass
+        self.cart_position_bins = np.linspace(-2.4, 2.4, 9)
+        self.cart_velovity_bins = np.linspace(-2, 2, 9)
+        self.pole_angle_bins = np.linspace(-0.4, 0.4, 9)
+        self.pole_velovity_bins = np.linspace(-3.5, 3.5, 9)
     
     def transform(self, parameter_list):
-        pass
+        cart_pos, cart_vel, pole_angle, pole_vel = observation
+        return build_state([
+            to_bin(cart_pos, self.cart_position_bins),
+            to_bin(cart_vel, self.cart_velovity_bins),
+            to_bin(pole_angle, self.pole_angle_bins),
+            to_bin(pole_vel, self.pole_velovity_bins),
+        ])
 
 class Model:
     def __init__(self, parameter_list):
