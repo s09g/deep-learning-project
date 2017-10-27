3df0410b7ec59cd5b946c02414d9725db42d66c5
zhangshiqiu <zhangshiqiu1993@126.com>
2017-10-27 10:36:13 -0700

    SGDRegressor

--- q_learning.py
+++ q_learning.py
@@ -11,3 +11,10 @@ class SGDRegressor:
     def __init__(self, D):
         self.w = np.random.randn(D) / np.sqrt(D)
         self.lr = 0.1
+
+    def partial_fit(self, X, Y):
+        self.w += self.lr * (Y - X.dot(self.w)).dot(X)
+
+    def predict(self, X):
+        return X.dot(self.w)
+    
