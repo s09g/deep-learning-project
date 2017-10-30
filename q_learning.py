bc4fd6a7321845e7de1d3eda0b2e4e6a5d7f56b6
zhangshiqiu <zhangshiqiu1993@126.com>
2017-10-29 19:58:52 -0700

    main function

--- q_learning.py
+++ q_learning.py
@@ -42,24 +42,89 @@ class FeatureTransformer:
         return self.featurizer.transfrom(scaled)
 
 class Model:
-  def __init__(self, env, feature_transformer):
-    self.env = env
-    self.models = []
-    self.feature_transformer = feature_transformer
-    for i in range(env.action_space.n):
-      model = SGDRegressor(feature_transformer.dimensions)
-      self.models.append(model)
+    def __init__(self, env, feature_transformer):
+        self.env = env
+        self.models = []
+        self.feature_transformer = feature_transformer
+        for i in range(env.action_space.n):
+            model = SGDRegressor(feature_transformer.dimensions)
+            self.models.append(model)
 
-  def predict(self, s):
-    X = self.feature_transformer.transform(np.atleast_2d(s))
-    return np.array([m.predict(X)[0] for m in self.models])
+    def predict(self, s):
+        X = self.feature_transformer.transform(np.atleast_2d(s))
+        return np.array([m.predict(X)[0] for m in self.models])
 
-  def update(self, s, a, G):
-    X = self.feature_transformer.transform(np.atleast_2d(s))
-    self.models[a].partial_fit(X, [G])
+    def update(self, s, a, G):
+        X = self.feature_transformer.transform(np.atleast_2d(s))
+        self.models[a].partial_fit(X, [G])
 
-  def sample_action(self, s, eps):
-    if np.random.random() < eps:
-      return self.env.action_space.sample()
-    else:
-      return np.argmax(self.predict(s))
+    def sample_action(self, s, eps):
+        if np.random.random() < eps:
+            return self.env.action_space.sample()
+        else:
+            return np.argmax(self.predict(s))
+
+
+def play_one(env, model, eps, gamma):
+    observation = env.reset()
+    done = False
+    totalreward = 0
+    iters = 0
+    while not done and iters < 2000:
+        # if we reach 2000, just quit, don't want this going forever
+        # the 200 limit seems a bit early
+        action = model.sample_action(observation, eps)
+        prev_observation = observation
+        observation, reward, done, info = env.step(action)
+
+        if done:
+            reward = -200
+
+        # update the model
+        next = model.predict(observation)
+        assert(len(next.shape) == 1)
+        G = reward + gamma*np.max(next)
+        model.update(prev_observation, action, G)
+
+        if reward == 1: # if we changed the reward to -200
+            totalreward += reward
+        iters += 1
+
+    return totalreward
+
+
+def main():
+    env = gym.make('CartPole-v0')
+    ft = FeatureTransformer(env)
+    model = Model(env, ft)
+    gamma = 0.99
+
+    if 'monitor' in sys.argv:
+        filename = os.path.basename(__file__).split('.')[0]
+        monitor_dir = './' + filename + '_' + str(datetime.now())
+        env = wrappers.Monitor(env, monitor_dir)
+
+
+    N = 500
+    totalrewards = np.empty(N)
+    costs = np.empty(N)
+    for n in range(N):
+        eps = 1.0/np.sqrt(n+1)
+        totalreward = play_one(env, model, eps, gamma)
+        totalrewards[n] = totalreward
+        if n % 100 == 0:
+            print("episode:", n, "total reward:", totalreward, "eps:", eps, "avg reward (last 100):", totalrewards[max(0, n-100):(n+1)].mean())
+
+    print("avg reward for last 100 episodes:", totalrewards[-100:].mean())
+    print("total steps:", totalrewards.sum())
+
+    plt.plot(totalrewards)
+    plt.title("Rewards")
+    plt.show()
+
+    plot_running_avg(totalrewards)
+
+
+if __name__ == '__main__':
+  main()
+
